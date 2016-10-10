a767cc5aa4bf7704ec8919f7d230f5b54386845c
rivten <rivten.grey@gmail.com>
2016-10-10 19:22:33 +0200

    [CAMERA] Creating a camera struct to hold details about camera

--- src/viewer.cpp
+++ src/viewer.cpp
@@ -22,6 +22,14 @@ void GameUpdateAndRender(thread_context* Thread, game_memory* Memory, game_input
 		State->BasicShader = LoadShader("../src/shaders/basic_v.glsl", "../src/shaders/basic_f.glsl");
 		State->Time = 0.0f;
 
+		State->Camera = {};
+		State->Camera.Pos = V3(0.0f, 0.0f, 5.0f);
+		State->Camera.Target = V3(0.0f, 0.0f, 0.0f);
+		State->Camera.FoV = Radians(45);
+		State->Camera.Aspect = float(GlobalWindowWidth) / float(GlobalWindowHeight);
+		State->Camera.NearPlane = 0.5f;
+		State->Camera.FarPlane = 30.0f;
+
 		// NOTE(hugo) : This must be the last command of the initialization of memory
 		Memory->IsInitialized = true;
 	}
@@ -32,83 +40,10 @@ void GameUpdateAndRender(thread_context* Thread, game_memory* Memory, game_input
 
 	Clear(V4(0.4f, 0.6f, 0.2f, 1.0f));
 
-#if 0
-	v3 Vertices[] = 
-	{
-	    V3(-1.0f, -1.0f,  1.0f), V3(1.0f, 0.0f, 0.0f),
-	    V3( 1.0f, -1.0f,  1.0f), V3(1.0f, 1.0f, 0.0f),
-	    V3(-1.0f,  1.0f,  1.0f), V3(0.0f, 1.0f, 0.0f),
-	    V3( 1.0f,  1.0f,  1.0f), V3(0.0f, 1.0f, 1.0f),
-	    V3(-1.0f, -1.0f, -1.0f), V3(0.0f, 0.0f, 1.0f),
-	    V3( 1.0f, -1.0f, -1.0f), V3(1.0f, 0.0f, 1.0f),
-	    V3(-1.0f,  1.0f, -1.0f), V3(1.0f, 1.0f, 1.0f),
-	    V3( 1.0f,  1.0f, -1.0f), V3(0.0f, 0.0f, 0.0f),
-	};
-
-	mat4 ModelMatrix = Translation(V3(0.5f, 0.0f, 0.0f)) * Rotation(GlobalTime, V3(0.0f, 1.0f, 0.0f)) * Scaling(V3(0.2f, 0.2f, 0.2f));
-	mat4 ViewMatrix = LookAt(V3(0.0f, 1.0f, 0.0f), V3(0.2f, 0.0f, 0.0f), V3(0.0f, 0.0f, 1.0f));
-	mat4 ProjectionMatrix = Perspective(Radians(60), GlobalWindowWidth / GlobalWindowHeight, 0.1f, 10.0f);
-
-	for(u32 VertexIndex = 0; VertexIndex < ArrayCount(Vertices); VertexIndex++)
-	{
-		if(VertexIndex % 2 == 0)
-		{
-			Vertices[VertexIndex] = (ProjectionMatrix * ViewMatrix * ModelMatrix * ToV4(Vertices[VertexIndex])).xyz;
-		}
-	}
-
-	GLuint Indices[] =
-	{
-		0, 1, 3,
-		0, 3, 2,
-		1, 5, 7,
-		1, 7, 3,
-		5, 4, 6,
-		5, 6, 7,
-		4, 0, 2,
-		4, 2, 6,
-		3, 6, 7,
-		3, 2, 6,
-		1, 4, 5,
-		1, 0, 4,
-	};
-
-	GLuint TriangleVAO;
-	glGenVertexArrays(1, &TriangleVAO);
-	GLuint TriangleVBO;
-	glGenBuffers(1, &TriangleVBO);
-	GLuint TriangleEBO;
-	glGenBuffers(1, &TriangleEBO);
-
-	glBindVertexArray(TriangleVAO);
-
-	glBindBuffer(GL_ARRAY_BUFFER, TriangleVBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, TriangleEBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
-	glEnableVertexAttribArray(0);
-
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-	glEnableVertexAttribArray(1);
-
-	glBindVertexArray(0);
-	shader BasicShader = shader("shaders/basic_v.glsl", "shaders/basic_f.glsl");
-
-	UseShader(BasicShader);
-	glBindVertexArray(TriangleVAO);
-	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
-	glBindVertexArray(0);
-#endif
-	//State->Mesh.ModelMatrix = Rotation(State->Time, V3(0.0f, 1.0f, 0.0f)) * Scaling(V3(0.2f, 0.2f, 0.2f));
 	State->Mesh.ModelMatrix = Scaling(V3(0.2f, 0.2f, 0.2f));
-	v3 CameraPos = V3(0.0f, 0.0f, 5.0f);
-	CameraPos = (Rotation(State->Time, V3(0.0f, 1.0f, 0.0f)) * ToV4(CameraPos)).xyz;
-	v3 CameraTarget = V3(0.0f, 0.0f, 0.0f);
-	mat4 ViewMatrix = LookAt(CameraPos, CameraTarget, V3(0.0f, 1.0f, 0.0f));
-	mat4 ProjectionMatrix = Perspective(Radians(45), float(GlobalWindowWidth) / float(GlobalWindowHeight), 0.5f, 30.0f);
+	v3 NewCameraPos = (Rotation(State->Time, V3(0.0f, 1.0f, 0.0f)) * ToV4(State->Camera.Pos)).xyz;
+	mat4 ViewMatrix = LookAt(NewCameraPos, State->Camera.Target, V3(0.0f, 1.0f, 0.0f));
+	mat4 ProjectionMatrix = Perspective(State->Camera.FoV, State->Camera.Aspect, State->Camera.NearPlane, State->Camera.FarPlane);
 
 	mat4 MVPMatrix = ProjectionMatrix * ViewMatrix * State->Mesh.ModelMatrix;
 	mat4 NormalMatrix = Transpose(Inverse(ViewMatrix * State->Mesh.ModelMatrix));
--- src/viewer.h
+++ src/viewer.h
@@ -1,9 +1,20 @@
 #pragma once
 
+struct camera
+{
+	v3 Pos;
+	v3 Target;
+	float FoV;
+	float Aspect;
+	float NearPlane;
+	float FarPlane;
+};
+
 struct game_state
 {
 	mesh Mesh;
 	shader BasicShader;
+	camera Camera;
 	float Time;
 };
 
