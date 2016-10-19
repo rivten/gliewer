80261f92a62b74c9c9a699229defd64f75b4cef2
rivten <rivten.grey@gmail.com>
2016-10-19 09:49:19 +0200

    [BUILD] Compiling with Muratori's ctime to profile build time

--- src/build.bat
+++ src/build.bat
@@ -1,10 +1,10 @@
 @echo off
 
-set STARTTIME=%TIME%
-
 rem --------------------------------------------------------------------------
 rem                        COMPILATION
 rem --------------------------------------------------------------------------
+
+ctime -begin gliewer_timings.ctm
 
 set SDLPath=C:\SDL2-2.0.4\
 set SDLBinPath=%SDLPath%\lib\x64\
@@ -25,34 +25,6 @@ popd
 rem --------------------------------------------------------------------------
 echo Compilation completed...
 
-rem Code used to be found here : http://stackoverflow.com/questions/9922498/calculate-time-difference-in-windows-batch-file
+ctime -end gliewer_timings.ctm
 
-set ENDTIME=%TIME%
-
-rem convert STARTTIME and ENDTIME to centiseconds
-set /A STARTTIME=(1%STARTTIME:~0,2%-100)*360000 + (1%STARTTIME:~3,2%-100)*6000 + (1%STARTTIME:~6,2%-100)*100 + (1%STARTTIME:~9,2%-100)
-set /A ENDTIME=(1%ENDTIME:~0,2%-100)*360000 + (1%ENDTIME:~3,2%-100)*6000 + (1%ENDTIME:~6,2%-100)*100 + (1%ENDTIME:~9,2%-100)
-
-rem calculating the duration is easy
-set /A DURATION=%ENDTIME%-%STARTTIME%
-
-rem we might have measured the time inbetween days
-if(%ENDTIME% LSS %STARTTIME%) set set /A DURATION=%STARTTIME%-%ENDTIME%
-
-rem now break the centiseconds down to hors, minutes, seconds and the remaining centiseconds
-set /A DURATIONH=%DURATION% / 360000
-set /A DURATIONM=(%DURATION% - %DURATIONH%*360000) / 6000
-set /A DURATIONS=(%DURATION% - %DURATIONH%*360000 - %DURATIONM%*6000) / 100
-set /A DURATIONHS=(%DURATION% - %DURATIONH%*360000 - %DURATIONM%*6000 - %DURATIONS%*100)
-
-rem some formatting
-if %DURATIONH% LSS 10 set DURATIONH=0%DURATIONH%
-if %DURATIONM% LSS 10 set DURATIONM=0%DURATIONM%
-if %DURATIONS% LSS 10 set DURATIONS=0%DURATIONS%
-if %DURATIONHS% LSS 10 set DURATIONHS=0%DURATIONHS%
-
-rem outputing
-echo %DURATIONH%h:%DURATIONM%m:%DURATIONS%s,%DURATIONHS%
-
-endlocal
-goto :EOF
+rem ctime -stats gliewer_timings.ctm
