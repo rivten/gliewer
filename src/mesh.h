a3b04d6146d4060581ed5f0fb2436fea9980112e
rivten <rivten.grey@gmail.com>
2016-12-13 11:40:47 +0100

    use of memory management for mesh loading

--- src/mesh.h
+++ src/mesh.h
@@ -3,15 +3,11 @@
 #define TINYOBJLOADER_IMPLEMENTATION
 
 #include <vector>
-#include <fstream>
-#include <unordered_map>
-#include <iostream>
 
-// TODO(hugo) : Find out how to get rid of this (but difficult problem since this isn't in the spec :( ... what's the most common case ?)
-#define OBJ_VERTEX_OFFSET 1
-#define GLIEWER_TRIANGLE_SUPPORT 1
-
-// TODO(hugo) : Un-std::hash this. I think std::vector are fine for the time being.
+// TODO(hugo) : I used a memory framework
+// which is not the Arena one (from Casey Muratori).
+// Maybe see if I can change the memory framework for
+// the latter
 
 /*
  * NOTE(hugo)
@@ -24,32 +20,20 @@ struct vertex
 	v3 Position;
 	v3 Normal;
 	v2 Texture;
-
-    bool operator==(const vertex& V) const
-    {
-        return(Position.x == V.Position.x &&
-           Position.y == V.Position.y &&
-           Position.z == V.Position.z &&
-           Normal.x == V.Normal.x &&
-           Normal.y == V.Normal.y &&
-           Normal.z == V.Normal.z &&
-           Texture.u == V.Texture.u &&
-           Texture.v == V.Texture.v);
-    }
-};
-
-struct hash_vertex
-{
-    // TODO(hugo) : BETTER HASH FUNCTIONS ! 
-    size_t operator()(const vertex& V) const
-    {
-        return (std::hash<float>()(V.Position.x) ^ std::hash<float>()(V.Position.y) ^ std::hash<float>()(V.Position.z));
-    }
 };
 
 struct triangle
 {
-	unsigned int Vertices[3];
+	union
+	{
+		struct
+		{
+			u32 Vertex0Index;
+			u32 Vertex1Index;
+			u32 Vertex2Index;
+		};
+		u32 VertexIndices[3];
+	};
 };
 
 struct mesh
@@ -60,24 +44,51 @@ struct mesh
 	GLuint EBO;
 
     // NOTE(hugo) : Mesh Data
-	std::vector<vertex> Vertices;
-	std::vector<triangle> Faces;
+	vertex* Vertices;
+	u32 VertexCount;
+	u32 VertexPoolSize;
+
+	triangle* Triangles;
+	u32 TriangleCount;
+	u32 TrianglePoolSize;
 
 	v4 Color;
 	std::string Name;
 };
 
+void PushVertex(mesh* Mesh, vertex V)
+{
+	if(Mesh->VertexCount == Mesh->VertexPoolSize)
+	{
+		Mesh->Vertices = ReAllocateArray(Mesh->Vertices, vertex, 2 * Mesh->VertexPoolSize);
+		Mesh->VertexPoolSize *= 2;
+	}
 
-// TODO(hugo) : maybe consider changing vec3 to vec4 (faster for OpenGL) ?
+	Mesh->Vertices[Mesh->VertexCount] = V;
+	++Mesh->VertexCount;
+}
+
+void PushTriangle(mesh* Mesh, triangle Triangle)
+{
+	if(Mesh->TriangleCount == Mesh->TrianglePoolSize)
+	{
+		Mesh->Triangles = ReAllocateArray(Mesh->Triangles, triangle, 2 * Mesh->TrianglePoolSize);
+		Mesh->TrianglePoolSize *= 2;
+	}
+
+	Mesh->Triangles[Mesh->TriangleCount] = Triangle;
+	++Mesh->TriangleCount;
+}
+
 void ComputeNormal(mesh* Mesh)
 {
 	Assert(Mesh != 0);
-	for (int TriangleIndex = 0; TriangleIndex < Mesh->Faces.size(); ++TriangleIndex)
+	for (u32 TriangleIndex = 0; TriangleIndex < Mesh->TriangleCount; ++TriangleIndex)
 	{
-		triangle t = Mesh->Faces[TriangleIndex];
-		v3 v0 = Mesh->Vertices[t.Vertices[0]].Position;
-		v3 v1 = Mesh->Vertices[t.Vertices[1]].Position;
-		v3 v2 = Mesh->Vertices[t.Vertices[2]].Position;
+		triangle t = Mesh->Triangles[TriangleIndex];
+		v3 v0 = Mesh->Vertices[t.VertexIndices[0]].Position;
+		v3 v1 = Mesh->Vertices[t.VertexIndices[1]].Position;
+		v3 v2 = Mesh->Vertices[t.VertexIndices[2]].Position;
 
 		// TODO(hugo) : investigate with the angle to see if I do not get a "negative" normal vector
 		// TODO(hugo) : need to normalize N after computation ?
@@ -88,12 +99,12 @@ void ComputeNormal(mesh* Mesh)
 			N = Normalized(CrossProduct);
 		}
 
-		for (int i = 0; i < 3; ++i)
+		for (u32 i = 0; i < 3; ++i)
 		{
-			Mesh->Vertices[t.Vertices[i]].Normal += N;
+			Mesh->Vertices[t.VertexIndices[i]].Normal += N;
 		}
 	}
-	for (int VertexIndex = 0; VertexIndex < Mesh->Vertices.size(); ++VertexIndex)
+	for (u32 VertexIndex = 0; VertexIndex < Mesh->VertexCount; ++VertexIndex)
 	{
 		if(LengthSqr(Mesh->Vertices[VertexIndex].Normal) != 0.0f)
 		{
@@ -105,46 +116,73 @@ void ComputeNormal(mesh* Mesh)
 void DrawTriangleMesh(mesh* Mesh)
 {
 	glBindVertexArray(Mesh->VAO);
-	glDrawElements(GL_TRIANGLES, (GLsizei)(3 * Mesh->Faces.size()), GL_UNSIGNED_INT, 0);
+	glDrawElements(GL_TRIANGLES, (GLsizei)(3 * Mesh->TriangleCount), GL_UNSIGNED_INT, 0);
 	glBindVertexArray(0);
 }
 
 void DrawWiredTriangleMesh(mesh* Mesh)
 {
 	glBindVertexArray(Mesh->VAO);
-	glDrawElements(GL_LINES, (GLsizei)(3 * Mesh->Faces.size()), GL_UNSIGNED_INT, 0);
+	glDrawElements(GL_LINES, (GLsizei)(3 * Mesh->TriangleCount), GL_UNSIGNED_INT, 0);
 	glBindVertexArray(0);
 }
 
-void DrawTriangleMeshInstances(mesh* Mesh, int InstanceCount)
+void DrawTriangleMeshInstances(mesh* Mesh, u32 InstanceCount)
 {
 	glBindVertexArray(Mesh->VAO);
-	glDrawElementsInstanced(GL_TRIANGLES, (GLsizei)(3 * Mesh->Faces.size()), GL_UNSIGNED_INT, 0, InstanceCount);
+	glDrawElementsInstanced(GL_TRIANGLES, (GLsizei)(3 * Mesh->TriangleCount), GL_UNSIGNED_INT, 0, InstanceCount);
 	glBindVertexArray(0);
 }
 
-void DrawWiredTriangleMeshInstances(mesh* Mesh, int InstanceCount)
+void DrawWiredTriangleMeshInstances(mesh* Mesh, u32 InstanceCount)
 {
 	glBindVertexArray(Mesh->VAO);
-	glDrawElementsInstanced(GL_LINES, (GLsizei)(3 * Mesh->Faces.size()), GL_UNSIGNED_INT, 0, InstanceCount);
+	glDrawElementsInstanced(GL_LINES, (GLsizei)(3 * Mesh->TriangleCount), GL_UNSIGNED_INT, 0, InstanceCount);
 	glBindVertexArray(0);
 }
+
+struct vertex_hash
+{
+	vertex V;
+	u32 VertexIndex;
+
+	vertex_hash* NextInHash;
+};
 
 // NOTE(hugo) : Lot of pressure on that function this is called three time for each face
-int GetPositionOfVertexInMesh(vertex V, mesh* Result, std::unordered_map<vertex, int, hash_vertex>& VertexIndices)
+u32 GetIndexOfVertexInMesh(vertex V, mesh* Mesh, vertex_hash** VertexHash, u32 HashBucketCount)
 {
-    // TODO(hugo) : Could this be improved using a hashmap of the index of the vertex in the OBJ positioning ?
-    std::unordered_map<vertex, int, hash_vertex>::const_iterator it = VertexIndices.find(V);
-    if(it == VertexIndices.end())
-    {
-        Result->Vertices.push_back(V);
-        VertexIndices[V] = (u32)(Result->Vertices.size() - 1);
-        return((u32)(Result->Vertices.size() - 1));
-    }
-    else
-    {
-        return(it->second);
-    }
+	// TODO(hugo) : Better hash functions !
+	u32 Hash = *(u32*)(void*)(&V.Position.x);
+	u32 HashBucket = Hash & (HashBucketCount - 1);
+	vertex_hash* Found = 0;
+
+	for(vertex_hash* Vertex = VertexHash[HashBucket]; Vertex; Vertex = Vertex->NextInHash)
+	{
+		if((Vertex->V.Position.x == V.Position.x) 
+				&& (Vertex->V.Position.y == V.Position.y) 
+				&& (Vertex->V.Position.z == V.Position.z))
+		{
+			// TODO(hugo) : This part has not been test because
+			// I did not try big enough meshes
+			Found = Vertex;
+			break;
+		}
+	}
+
+	if(!Found)
+	{
+		PushVertex(Mesh, V);
+		Found = AllocateStruct(vertex_hash);
+		*Found = {};
+		Found->V = V;
+		Found->VertexIndex = Mesh->VertexCount - 1;
+		Found->NextInHash = VertexHash[HashBucket];
+
+		VertexHash[HashBucket] = Found;
+	}
+
+	return(Found->VertexIndex);
 }
 
 void GenerateDataBuffer(mesh* Mesh)
@@ -157,10 +195,10 @@ void GenerateDataBuffer(mesh* Mesh)
 
 	glBindVertexArray(Mesh->VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, Mesh->VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex) * Mesh->Vertices.size(), (GLuint*)Mesh->Vertices.data(), GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex) * Mesh->VertexCount, (GLuint*)Mesh->Vertices, GL_STATIC_DRAW);
 
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, Mesh->EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(triangle) * Mesh->Faces.size(), (GLuint*)Mesh->Faces.data(), GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(triangle) * Mesh->TriangleCount, (GLuint*)Mesh->Triangles, GL_STATIC_DRAW);
 	
 	// NOTE(hugo) : Vertex positions
 	glEnableVertexAttribArray(0);
@@ -177,7 +215,6 @@ void GenerateDataBuffer(mesh* Mesh)
 	glBindVertexArray(0);
 }
 
-#if 1
 // ---------------------------------------
 // NOTE(hugo) : TinyObjLoader mesh implementation
 // ---------------------------------------
@@ -197,6 +234,12 @@ std::vector<mesh> LoadOBJ(const std::string BaseDir, const std::string Filename)
 	for(u32 ShapeIndex = 0; ShapeIndex < Shapes.size(); ++ShapeIndex)
 	{
 		mesh Mesh = {};
+		Mesh.VertexCount = 0;
+		Mesh.VertexPoolSize = 1;
+		Mesh.Vertices = AllocateArray(vertex, Mesh.VertexPoolSize);
+		Mesh.TriangleCount = 0;
+		Mesh.TrianglePoolSize = 1;
+		Mesh.Triangles = AllocateArray(triangle, Mesh.TrianglePoolSize);
 		Mesh.Name = Shapes[ShapeIndex].name;
 		Mesh.Color = V4(1.0f, 1.0f, 1.0f, 1.0f);
 		for(u32 MaterialIndex = 0; MaterialIndex < Materials.size(); ++MaterialIndex)
@@ -217,7 +260,10 @@ std::vector<mesh> LoadOBJ(const std::string BaseDir, const std::string Filename)
 		}
 
 		bool NormalsComputed = true;
-		std::unordered_map<vertex, int, hash_vertex> VertexIndices;
+
+		// NOTE(hugo) : Must be a power of two
+		vertex_hash* VertexHash[8];
+		ZeroStruct(VertexHash);
 		for(u32 TriangleIndex = 0; TriangleIndex < (Shapes[ShapeIndex].mesh.indices.size() / 3); ++TriangleIndex)
 		{
 			triangle Triangle = {};
@@ -251,11 +297,24 @@ std::vector<mesh> LoadOBJ(const std::string BaseDir, const std::string Filename)
 							Attributes.texcoords[2 * TextureIndex + 1]);
 				}
 
-				int VertexIndexInMesh = GetPositionOfVertexInMesh(V, &Mesh, VertexIndices);
-				Triangle.Vertices[i] = VertexIndexInMesh;
+				u32 VertexIndexInMesh = GetIndexOfVertexInMesh(V, &Mesh, VertexHash, ArrayCount(VertexHash));
+				Triangle.VertexIndices[i] = VertexIndexInMesh;
 			}
-			Mesh.Faces.push_back(Triangle);
+			PushTriangle(&Mesh, Triangle);
 		}
+
+		// TODO(hugo) : Freeing vertex hash. Am I sure there is not a memory leak ??
+		for(u32 VertexHashIndex = 0; VertexHashIndex < ArrayCount(VertexHash); ++VertexHashIndex)
+		{
+			vertex_hash* Vertex = VertexHash[VertexHashIndex];
+			while(Vertex)
+			{
+				vertex_hash* Next = Vertex->NextInHash;
+				Free(Vertex);
+				Vertex = Next;
+			}
+		}
+
 		if(!NormalsComputed)
 		{
 			ComputeNormal(&Mesh);
@@ -269,321 +328,3 @@ std::vector<mesh> LoadOBJ(const std::string BaseDir, const std::string Filename)
 	return(Result);
 }
 
-#else
-// ---------------------------------------
-// NOTE(hugo) : My own mesh implementation
-// ---------------------------------------
-
-mesh LoadOFF(const std::string& filename)
-{
-	mesh Result;
-	Result.Faces = std::vector<triangle>();
-	Result.Vertices = std::vector<vertex>();
-
-	std::ifstream File;
-	File.open(filename.c_str(), std::fstream::in);
-	if (!File.is_open())
-	{
-		//TODO(hugo)
-		return(Result);
-	}
-
-	std::string Line;
-	std::getline(File, Line); // NOTE(hugo) : 'OFF' line
-
-	std::string VerticesNumberString;
-	File >> VerticesNumberString;
-	int VerticesNumber = std::stoi(VerticesNumberString);
-
-	std::string FacesNumberString;
-	File >> FacesNumberString;
-	int FacesNumber = std::stoi(FacesNumberString);
-
-	std::string Value;
-	File >> Value; // NOTE(hugo) : Number of edges (useless for us)
-	for (int VertexIndex = 0; VertexIndex < VerticesNumber; ++VertexIndex)
-	{
-		vertex V;
-		File >> Value;
-		V.Position.x = std::stof(Value);
-		File >> Value;
-		V.Position.y = std::stof(Value);
-		File >> Value;
-		V.Position.z = std::stof(Value);
-
-		V.Normal = V3(0.0f, 0.0f, 0.0f);
-
-		Result.Vertices.push_back(V);
-	}
-
-	for (int FaceIndex = 0; FaceIndex < FacesNumber; ++FaceIndex)
-	{
-		File >> Value;
-		int FaceDegree = std::stoi(Value);
-
-		Assert(FaceDegree == 3); //NOTE(hugo): I only consider faces which are triangles
-		triangle T;
-		File >> Value;
-		T.Vertices[0] = std::stoi(Value);
-		File >> Value;
-		T.Vertices[1] = std::stoi(Value);
-		File >> Value;
-		T.Vertices[2] = std::stoi(Value);
-		Result.Faces.push_back(T);
-	}
-
-	ComputeNormal(&Result);
-	glGenVertexArrays(1, &Result.VAO);
-
-	glGenBuffers(1, &Result.VBO);
-
-	glGenBuffers(1, &Result.EBO);
-
-	glBindVertexArray(Result.VAO);
-	glBindBuffer(GL_ARRAY_BUFFER, Result.VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex) * Result.Vertices.size(), &Result.Vertices[0], GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, Result.EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * 3 * Result.Faces.size(), (GLuint*)Result.Faces.data(), GL_STATIC_DRAW);
-	
-	// NOTE(hugo) : Vertex positions
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(vertex), (GLvoid*)0);
-
-	// NOTE(hugo) : Vertex normals
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(vertex), (GLvoid*)offsetof(vertex, Normal));
-
-	glBindVertexArray(0);
-
-	return(Result);
-}
-
-struct parse_obj_triangle_result
-{
-    bool PositionOnly;
-    bool UsingTextures;
-    int VertexIndex;
-    int NormalIndex;
-    int TextureIndex;
-};
-
-parse_obj_triangle_result ParseOBJTriangle(const std::string TriangleString)
-{
-    parse_obj_triangle_result Result = {};
-    int VertexPosition = (u32)(TriangleString.find('/'));
-    Result.PositionOnly = (VertexPosition == -1);
-    std::string VertexIndexString = TriangleString.substr(0, VertexPosition);
-
-    Result.VertexIndex = std::stoi(VertexIndexString) - OBJ_VERTEX_OFFSET;
-    // TODO(hugo) : Maybe not optimal, I should consider using several types for classifying the mesh I'm currently parsing
-    if(!Result.PositionOnly)
-    {
-        int TexturePosition = (u32)(TriangleString.find('/', VertexPosition + 1));
-        std::string TextureIndexString = TriangleString.substr(VertexPosition + 1, TexturePosition - VertexPosition - 1);
-        std::string NormalIndexString = TriangleString.substr(TexturePosition + 1);
-
-        if(TextureIndexString != "")
-        {
-            Result.TextureIndex = std::stoi(TextureIndexString) - OBJ_VERTEX_OFFSET;
-            Result.UsingTextures = true;
-        }
-        else
-        {
-            Result.UsingTextures = false;
-        }
-        Result.NormalIndex = std::stoi(NormalIndexString) - OBJ_VERTEX_OFFSET;
-    }
-    return(Result);
-}
-
-vertex CreateVertexFromTriangleString(std::string TriangleString, 
-        std::vector<v3>& Positions, 
-        std::vector<v3>& Normals, 
-        std::vector<v2>& Textures)
-{
-    parse_obj_triangle_result ParsedTriangle = ParseOBJTriangle(TriangleString);
-    vertex V = {};
-
-    V.Position= Positions[ParsedTriangle.VertexIndex];
-    if(!ParsedTriangle.PositionOnly)
-    {
-        if(ParsedTriangle.UsingTextures)
-        {
-            V.Texture = Textures[ParsedTriangle.TextureIndex];
-        }
-        V.Normal = Normals[ParsedTriangle.NormalIndex];
-    }
-    return(V);
-}
-
-mesh LoadOBJ(const std::string filename)
-{
-    // NOTE(hugo) : This OBJ parser was implemented with the use of this spec :
-    // www.martinreddy.net/gfx/3d/obj.spec
-    mesh Result = {};
-    std::vector<v3> Positions;
-    std::vector<v3> Normals;
-    std::vector<v2> Textures;
-    std::ifstream File;
-    std::unordered_map<vertex, int, hash_vertex> VertexIndices;
-#if defined GLIEWER_DEBUG
-    int DEBUGLineIndex = 0;
-#endif
-    File.open(filename.c_str(), std::fstream::in);
-    if(!File.is_open())
-    {
-        // TODO(hugo)
-		SDL_Log("Error, OBJ file not found");
-        return(Result);
-    }
-
-    while(!File.eof())
-    {
-        // NOTE(hugo) : One loop must process one entire line
-        std::string Word;
-        File >> Word;
-        if(Word == "#")
-        {
-            // NOTE(hugo) : Comment. Ignore the rest of the line
-            std::getline(File, Word);
-            Word.clear();
-        }
-        else if(Word == "v")
-        {
-            float X, Y, Z, W;
-            W = 1.0f;
-            File >> X;
-            File >> Y;
-            File >> Z;
-            char NextChar;
-            File.get(NextChar);
-            if(NextChar == '\n')
-            {
-                // NOTE(hugo) : No W was set
-            }
-            else
-            {
-                File >> W;
-                Assert(W != 0.0f);
-            }
-            v3 Vertex = V3(X / W, Y / W, Z / W);
-            Positions.push_back(Vertex);
-        }
-        else if(Word == "vt")
-        {
-            // NOTE(hugo) : We support only 2D textures for now
-            float U, V;
-            File >> U;
-            File >> V;
-            v2 VertexTexture = V2(U, V);
-            Textures.push_back(VertexTexture);
-        }
-        else if(Word == "vn")
-        {
-            float X, Y, Z;
-            File >> X;
-            File >> Y;
-            File >> Z;
-            v3 VertexNormal = V3(X, Y, Z);
-            Normals.push_back(VertexNormal);
-        }
-        else if(Word == "f")
-        {
-#if !GLIEWER_TRIANGLE_SUPPORT
-            int LastVertexIndices[2] = {};
-#endif
-            triangle Triangle;
-            std::string TriangleString;
-            for(int Index = 0; Index < 3; ++Index)
-            {
-                File >> TriangleString; // NOTE(hugo) : Triangle string is of the form V/VT/VN or V//VN or V
-                vertex V = CreateVertexFromTriangleString(TriangleString, Positions, Normals, Textures);
-
-                int VertexIndexInMesh = GetPositionOfVertexInMesh(V, &Result, VertexIndices);
-                Triangle.Vertices[Index] = VertexIndexInMesh;
-            }
-
-#if !GLIEWER_TRIANGLE_SUPPORT
-            LastVertexIndices[0] = Triangle.Vertices[1];
-            LastVertexIndices[1] = Triangle.Vertices[2];
-#endif
-            Result.Faces.push_back(Triangle);
-
-#if !GLIEWER_TRIANGLE_SUPPORT
-            LastVertexIndices[0] = Triangle.Vertices[1];
-            // NOTE(hugo) : Support for polygon with more than 3 vertices
-            char NextChar;
-            File.get(NextChar);
-
-            while(NextChar != '\n')
-            {
-                // NOTE(hugo) : Go to the next place that isn't a space
-                while(NextChar == ' ')
-                {
-                    File.get(NextChar);
-                }
-                if(NextChar == '\n')
-                {
-                    break;
-                }
-
-                File.unget();
-                File >> TriangleString; 
-                vertex V = CreateVertexFromTriangleString(TriangleString, Positions, Normals, Textures);
-
-                // NOTE(hugo) : I handle the polygon in a very special scheme since I could not find documentation about this in the specs.
-                // I assume that the vertex v0, v1, v2 forms the first triangle. Then its v1, v2, v3. etc...
-                // But it could (for example) be : v0, v1, v2 then v0, v2, v3, etc...
-                int VertexIndexInMesh = GetPositionOfVertexInMesh(V, &Result, VertexIndices);
-                Triangle.Vertices[0] = LastVertexIndices[0];
-                Triangle.Vertices[1] = LastVertexIndices[1];
-                Triangle.Vertices[2] = VertexIndexInMesh;
-
-                LastVertexIndices[0] = LastVertexIndices[1];
-                LastVertexIndices[1] = VertexIndexInMesh;
-
-                File.get(NextChar);
-            }
-#else
-            // NOTE(hugo) : Ignore the rest of the line
-            std::getline(File, Word);
-#endif
-        }
-        /*
-         * TODO(hugo) : ALL THESE ! This include parsing and rendering according to MTL and TGA files !!
-        else if(Word == "g")
-        {
-        }
-        else if(Word == "s")
-        {
-        }
-        else if(Word == "o")
-        {
-        }
-        else if(Word == "mtllib")
-        {
-        }
-        else if(Word == "usemtl")
-        {
-        }
-        */
-        else
-        {
-            // NOTE(hugo) : Ignore the rest of the line
-            std::getline(File, Word);
-        }
-#if defined GLIEWER_DEBUG
-        DEBUGLineIndex++;
-#endif
-    }
-
-    // TODO(hugo) : We must compute normals only if : (a) users requires a normalized mesh, (b) OBJ file does not give us normals
-    ComputeNormal(&Result);
-
-    // TODO(hugo) : Here we assume that the normals were given ! Do not do this in the future
-
-    return(Result);
-}
-
-#endif
--- src/rivten.h
+++ src/rivten.h
@@ -28,7 +28,6 @@ typedef size_t memory_index;
 
 #define ArrayCount(x) (sizeof((x))/(sizeof((x)[0])))
 #define Assert(x) do{if(!(x)){*(int*)0=0;}}while(0)
-#define CopyArray(Source, Dest) for(int Index = 0; Index < ArrayCount((Source)); ++Index){(Dest)[Index] = (Source)[Index];}
 
 #define global_variable static
 // TODO(hugo) : Find out how to restablish this #define
@@ -44,3 +43,96 @@ typedef size_t memory_index;
 #define Gigabytes(Value) (Megabytes(Value)*1024LL)
 #define Terabytes(Value) (Gigabytes(Value)*1024LL)
 
+#define ZeroStruct(Instance) ZeroSize(sizeof(Instance), &(Instance))
+void ZeroSize(memory_index Size, void* Ptr)
+{
+	u8* Byte = (u8 *)Ptr;
+	while(Size > 0)
+	{
+		*Byte = 0;
+		++Byte;
+		--Size;
+	}
+}
+
+// TODO(hugo) : To get rid of this in order to use only arena framework ?
+// {
+#include <stdlib.h>
+#define ReAllocateArray(Buffer, type, Size) (type *)ReAllocate_(Buffer, Size * sizeof(type))
+#define AllocateArray(type, Size) (type *)Allocate_(Size * sizeof(type))
+#define AllocateStruct(type) AllocateArray(type, 1)
+#define Free(Buffer) free(Buffer)
+#define CopyArray(Dest, Source, type, Size) memcpy(Dest, Source, Size * sizeof(type))
+
+void* Allocate_(size_t Size)
+{
+	void* Result = malloc(Size);
+	Assert(Result);
+
+	return(Result);
+}
+
+void* ReAllocate_(void* Buffer, size_t Size)
+{
+	void* Result = realloc(Buffer, Size);
+	Assert(Result);
+
+	return(Result);
+}
+// }
+
+struct memory_arena
+{
+	memory_index Size;
+	memory_index Used;
+	u8* Base;
+
+	u32 TemporaryCount;
+};
+
+struct temporary_memory
+{
+	memory_arena* Arena;
+	memory_index Used;
+};
+
+void InitializeArena(memory_arena* Arena, memory_index Size, void* Base)
+{
+	Arena->Size = Size;
+	Arena->Used = 0;
+	Arena->TemporaryCount = 0;
+	Arena->Base = (u8 *)Base;
+}
+
+#define PushStruct(Arena, type) (type *)PushSize_(Arena, sizeof(type))
+#define PushArray(Arena, Count, type) (type *)PushSize_(Arena, (Count)*sizeof(type))
+#define PushSize(Arena, Size) PushSize_(Arena, Size)
+void* PushSize_(memory_arena* Arena, memory_index Size)
+{
+	Assert(Arena->Used + Size <= Arena->Size);
+	void* Result = Arena->Base + Arena->Used;
+	Arena->Used += Size;
+
+	return(Result);
+}
+
+temporary_memory BeginTemporaryMemory(memory_arena* Arena)
+{
+	temporary_memory Result = {};
+
+	Result.Arena = Arena;
+	Result.Used = 0;
+
+	++Arena->TemporaryCount;
+
+	return(Result);
+}
+
+void EndTemporaryMemory(temporary_memory TemporaryMemory)
+{
+	memory_arena* Arena = TemporaryMemory.Arena;
+	Assert(Arena->Used >= TemporaryMemory.Used);
+	Arena->Used -= TemporaryMemory.Used;
+	Assert(Arena->TemporaryCount > 0);
+	--Arena->TemporaryCount;
+}
