b0ed38b7d2f3114805b18783b0a3afbd8d3cd84c
rivten <rivten.grey@gmail.com>
2017-03-24 17:20:01 +0100

    rename the executable

--- src/build.sh
+++ src/build.sh
@@ -4,5 +4,5 @@
 #g++ -g -std=c++11 -O2 -Wno-write-strings -I ../dep/imgui/ -I ../dep/tinyobjloader/ -I ../dep/stb/ sdl_gl_layer.cpp -l SDL2 -l GL -l GLEW -D RIVTEN_SLOW=1 -D GLIEWER_DEBUG=1 -o ../build/gliewer.sh
 
 # NOTE(hugo) : SLOW EXECUTABLE
-g++ -g -std=c++11 -Wno-write-strings -I ../dep/imgui/ -I ../dep/tinyobjloader/ -I ../dep/stb/ sdl_gl_layer.cpp -l SDL2 -l GL -l GLEW -D RIVTEN_SLOW=1 -D GLIEWER_DEBUG=1 -o ../build/gliewer.sh
+g++ -g -std=c++11 -Wno-write-strings -I ../dep/imgui/ -I ../dep/tinyobjloader/ -I ../dep/stb/ sdl_gl_layer.cpp -l SDL2 -l GL -l GLEW -D RIVTEN_SLOW=1 -D GLIEWER_DEBUG=1 -o ../build/gliewer
 ~/dev/ctime/ctime -end gliewer_timings.ctm
